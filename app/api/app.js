5562db235c062f069efbfa679e26e38395e65749
Sourabh Yogi <sorabh.yogi06@gmail.com>
2026-08-07 15:31:44 +0530

    added config

--- app/api/app.js
+++ app/api/app.js
@@ -19,7 +19,7 @@ app.get('/api/status', function(req, res, next) {
   pool.connect((err, client, release) => {
     if (err) {
       console.error('Error acquiring client', err.stack);
-      return next(err);   // let Express error handler respond, don't hang
+      return next(err);
     }
     client.query('SELECT now() as time', (err, result) => {
       release();
@@ -31,3 +31,36 @@ app.get('/api/status', function(req, res, next) {
     });
   });
 });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
